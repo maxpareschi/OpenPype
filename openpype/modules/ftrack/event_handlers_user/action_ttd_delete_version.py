6992570433fe4102ab9abcc1a5de698a6267cf0a
jm22dogs <jmoraga.martin@22dogstudio.com>
2024-05-28 15:47:17 +0100

    adding support for several versions and lists adding also a pop up with the versions that will be removed

--- openpype/modules/ftrack/event_handlers_user/action_ttd_delete_version.py
+++ openpype/modules/ftrack/event_handlers_user/action_ttd_delete_version.py
@@ -141,8 +141,9 @@ class DeleteVersionAction(BaseAction):
     description = 'Delete version action'
     priority = 10000
     # role_list = ['Pypeclub']
-    icon = statics_icon("ftrack", "action_icons", "delete_version.svg")
+    icon = statics_icon("ftrack", "action_icons", "delete_version.png")
     query_items = "asset.parent, asset.parent.custom_attributes, asset.name, version"
+    settings_key = "delivery_action"
 
 
     def __init__(self, *args, **kwargs):
@@ -153,51 +154,154 @@ class DeleteVersionAction(BaseAction):
 
 
     def discover(self, session: Session, entities: List[Entity], event: Event):
-        if len(entities) != 1:
-            return False
-        elif not isinstance(entities[0], AssetVersion):
-            return False
-        return True
+        is_valid = False
+        for entity in entities:
+            if entity.entity_type.lower() in ("assetversion", "reviewsession", "assetversionlist"):
+                is_valid = True
+                break
+
+        if is_valid:
+            is_valid = self.valid_roles(session, entities, event)
+        self.versions = None
+        return is_valid
+
+
+    def get_interface(self):
+        gui = [
+            {"type":"label",
+            "value": "<h2><b>WARNING</b>: The following versions will be removed from Ftrack and OpenPype.<br>"
+            "If you want to cancel, press the <b>X</b> button in the upper right corner.</h2>"},
+            {"type": "hidden", "name":"hidden", "value": True}
+        ]
+        vlist = "<ul>"
+        for v in self.versions:
+            name = f"<h3>{v['asset']['parent']['name']} - {v['asset']['name']} - {v['version']}</h3>"
+            vlist += f"<li>{name}</i>"
+        gui.append({
+            "type":"label",
+            "value": vlist + "</ul>"})
+
+        return gui
+
 
     def launch(self, session: Session, entities: List[Entity], event: Event):
         # from pprint import pprint
-        entity = entities[0]
-        self.log.info(f"Working on version {entity['asset']['name']}")
-        in_links = list(entity["incoming_links"])
-        out_links = list(entity["outgoing_links"])
+        self.versions = self.versions or self._extract_asset_versions(session, entities)
 
-        if out_links and in_links:
-            msg = f"Incoming and outgoing links should not happen simultaneously."
-            raise NotImplementedError(msg)
+        if not event["data"].get("values", {}):
+            return {
+                "type": "form",
+                "items": self.get_interface(),
+                "title": "Confirm",
+                "submit_button_label": "I know what I'm doing. <b>Remove permanently.</b>"
+                }
 
-        version_ids = [entity["id"]]
+        for entity in self.versions:
+            self.log.info(f"Working on version {entity['asset']['name']}")
+            in_links = list(entity["incoming_links"])
+            out_links = list(entity["outgoing_links"])
 
-        if in_links:  # is delivery version
-            self.log.info("Working on delivery version")
+            if out_links and in_links:
+                msg = f"Incoming and outgoing links should not happen simultaneously."
+                raise NotImplementedError(msg)
 
-            # clear source attributes
-            in_links[0]["from"]["custom_attributes"]["client_version_string"] = ""
+            version_ids = [entity["id"]]
 
-        elif out_links:  # has delivery version
-            self.log.info("Working on normal version with delivery attached")
+            if in_links:  # is delivery version
+                self.log.info("Working on delivery version")
 
-            # fetch delivery version and mark it for deletion
-            delivery_version = out_links[0]["to"]
-            version_ids.append(delivery_version["id"])
+                # clear source attributes
+                in_links[0]["from"]["custom_attributes"]["client_version_string"] = ""
 
-        else:  # just remove this one, don't do anything else
-            self.log.info("Working on normal version")
+            elif out_links:  # has delivery version
+                self.log.info("Working on normal version with delivery attached")
 
-        ids = ", ".join(version_ids)
-        query = f"select {self.query_items} from AssetVersion where id in ({ids})"
-        versions_to_delete = self.session.query(query).all()
-        delete_versions(versions_to_delete)
-        for version in versions_to_delete:
-            session.delete(version)
-        session.commit()
-        msg = f"Removing versions {[v['id'] for v in versions_to_delete]}"
-        return { "success" : True, "message" : msg}
+                # fetch delivery version and mark it for deletion
+                delivery_version = out_links[0]["to"]
+                version_ids.append(delivery_version["id"])
 
+            else:  # just remove this one, don't do anything else
+                self.log.info("Working on normal version")
+
+            ids = ", ".join(version_ids)
+            query = f"select {self.query_items} from AssetVersion where id in ({ids})"
+            versions_to_delete = self.session.query(query).all()
+            if not versions_to_delete:
+                continue
+            delete_versions(versions_to_delete)
+            for version in versions_to_delete:
+                session.delete(version)
+            session.commit()
+            msg = f"Removing versions {[v['id'] for v in versions_to_delete]}"
+        # return { "success" : True, "message" : msg}
+
+    def _extract_asset_versions(self, session: Session, entities: List[Entity]):
+        asset_version_ids = set()
+        review_session_ids = set()
+        asset_version_list_ids = set()
+
+        for entity in entities:
+            entity_type_low = entity.entity_type.lower()
+
+            if entity_type_low == "assetversion":
+                asset_version_ids.add(entity["id"])
+
+            elif entity_type_low == "reviewsession":
+                review_session_ids.add(entity["id"])
+
+            elif entity_type_low == "assetversionlist":
+                asset_version_list_ids.add(entity["id"])
+
+        for version_id in self._get_asset_version_ids_from_asset_ver_list(
+            session, asset_version_list_ids
+        ):
+            asset_version_ids.add(version_id)
+
+        for version_id in self._get_asset_version_ids_from_review_sessions(
+            session, review_session_ids
+        ):
+            asset_version_ids.add(version_id)
+
+        qkeys = self.join_query_keys(asset_version_ids)
+        query = "select id, version, asset_id, incoming_links, outgoing_links"
+        query += f" from AssetVersion where id in ({qkeys})"
+
+        asset_versions = session.query(query).all()
+        for version in [*asset_versions]:
+            if version["outgoing_links"] and not version["incoming_links"]:
+                delivery = version["outgoing_links"][0]["to"]
+                if delivery in asset_versions:
+                    continue
+                asset_versions.append(delivery)
+                self.log.info(f"Appending version source {delivery}")
+
+        return asset_versions
+
+    def _get_asset_version_ids_from_review_sessions(
+        self, session, review_session_ids
+    ):
+        if not review_session_ids:
+            return set()
+        review_session_objects = session.query((
+            "select version_id from ReviewSessionObject"
+            " where review_session_id in ({})"
+        ).format(self.join_query_keys(review_session_ids))).all()
+
+        return {
+            review_session_object["version_id"]
+            for review_session_object in review_session_objects
+        }
+
+    def _get_asset_version_ids_from_asset_ver_list( self, session, asset_ver_list_ids):
+        # this can be static method..
+        if not asset_ver_list_ids:
+            return set()
+
+        ids = ", ".join(asset_ver_list_ids)
+        query_str = f"select id from AssetVersion where lists any (id in ({ids}))"
+        asset_versions = session.query(query_str).all()
+
+        return {asset_version["id"] for asset_version in asset_versions}
 
 
 def register(session):
