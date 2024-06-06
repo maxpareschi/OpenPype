54890be147a72ae81cfe92e1acc999b8940863e3
maxpareschi <max.pareschi@gmail.com>
2024-06-06 23:22:43 +0200

    gather options and collect data for farm

--- openpype/plugins/publish/collect_gather_data.py
+++ openpype/plugins/publish/collect_gather_data.py
@@ -36,8 +36,24 @@ class CollectGatherData(pyblish.api.InstancePlugin):
             return None
 
     def process(self, instance):
-
+        import json
         context = instance.context
+
+        publish_options = context.data["publish_attributes"]        
+        gather_options = publish_options["CollectGatherOptions"]
+        self.log.debug("Gather publish options: '{}'".format(
+            json.dumps(gather_options, indent=4, default=str)))
+        
+        if gather_options["gather_on_farm"]:
+            instance.data["families"].append("gather.farm")
+            instance.data["publish_on_farm"] = True
+            self.log.debug("Instance 'families': '{}'".format(
+                instance.data["families"]
+            ))
+            self.log.debug("Instance 'publish_on_farm': '{}'".format(
+                instance.data["publish_on_farm"]
+            ))
+
         gather_settings = context.data["project_settings"]["ftrack"]["user_handlers"]["gather_action"]
         min_version = gather_settings["min_gather_version"]
         missing_task_version = gather_settings["missing_task_gather_version"]
