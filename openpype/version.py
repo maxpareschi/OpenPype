c8427dbe2a15c207844beba75a94580739321972
maxpareschi <max.pareschi@gmail.com>
2024-06-19 11:04:34 +0200

    bump version to v64

--- openpype/version.py
+++ openpype/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 """Package declaring Pype version."""
-__version__ = "3.14.11-22dogs.63"
-# __version__ = "3.14.11-22dogs.71+staging"
+__version__ = "3.14.11-22dogs.64"
+# __version__ = "3.14.11-22dogs.72+staging"
