4e75eee2d4f99d3ce0a0e59adc9d99f50e12d22e
maxpareschi <max.pareschi@gmail.com>
2024-06-22 13:58:06 +0200

    bump version to v68 - v75+staging

--- openpype/version.py
+++ openpype/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 """Package declaring Pype version."""
-__version__ = "3.14.11-22dogs.67"
-# __version__ = "3.14.11-22dogs.73+staging"
+__version__ = "3.14.11-22dogs.68"
+# __version__ = "3.14.11-22dogs.75+staging"
