1f74aeaacb691b7a4223ff46587c33346b36ae21
jm22dogs <jmoraga.martin@22dogstudio.com>
2024-07-02 08:30:37 +0100

    better python: the update method receives now the ApplicationLaunchContext class so that it is more explicit what this function does.

--- openpype/hosts/houdini/hooks/set_custom_envars.py
+++ openpype/hosts/houdini/hooks/set_custom_envars.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
+
 from openpype.lib import PreLaunchHook
 from openpype.client import get_asset_by_name
+from openpype.lib.applications import ApplicationLaunchContext
 
 
-def update_custom_environ_vars(env: dict):
+def update_application_launch_ctx(launch_ctx: ApplicationLaunchContext):
+    """Adds SFSTART and SFEND to the given OpenPype app launch context."""
+
+    env = launch_ctx.env
     ctx = get_asset_by_name(env["AVALON_PROJECT"], env["AVALON_ASSET"])
     env["SFSTART"] = str(ctx['data']['frameStart'] - ctx['data']['handleStart'])
     env["SFEND"] = str(ctx['data']['frameEnd'] + ctx['data']['handleEnd'])
@@ -13,6 +18,6 @@ class SetCustomEnvVars(PreLaunchHook):
     app_groups = ["houdini"]
 
     def execute(self):
+        update_application_launch_ctx(self.launch_context)
         env = self.launch_context.env
-        update_custom_environ_vars(env)
         print(f"Adding env var SFSTART: {env['SFSTART']} and SFEND: {env['SFEND']}")
