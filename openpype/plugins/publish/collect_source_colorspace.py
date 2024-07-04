eb0210bb38de45050368b08a73edfec552fd4b27
maxpareschi <max.pareschi@gmail.com>
2024-07-04 23:49:27 +0200

    Collect source colorspace, new plugin that gets colorspace from version coming from publish available in instance

---
+++ openpype/plugins/publish/collect_source_colorspace.py
@@ -0,0 +1,46 @@
+import pyblish.api
+
+from openpype.client import (
+    get_asset_by_name,
+    get_subset_by_name,
+    get_last_version_by_subset_id,
+)
+
+class CollectSourceColorspace(pyblish.api.InstancePlugin):
+    """
+    Collects source colorspace from plateMain if present,
+    otherwise defaults to None
+    """
+    label = "Collect source colorspace"
+    order = pyblish.api.CollectorOrder + 0.49925
+    families = [
+        "review",
+        "render",
+        "gather"
+    ]
+
+    def process(self, instance):
+
+        if instance.data.get("farm", None):
+            self.log.info("Farm mode is on, skipping.")
+            return
+
+        context = instance.context
+  
+        asset_doc = get_asset_by_name(context.data["projectName"],
+                                      instance.data["asset"],
+                                      fields=["_id"])
+        if asset_doc:
+            subset_doc = get_subset_by_name(context.data["projectName"],
+                                            "plateMain",
+                                            asset_doc["_id"],
+                                            fields=["_id"])
+        if subset_doc:
+            version_doc = get_last_version_by_subset_id(context.data["projectName"],
+                                                        subset_doc["_id"],
+                                                        fields=["_id", "data"])
+        
+        if version_doc:
+            instance.data["colorspace"] = version_doc["data"]["colorspace"]
+        else:
+            instance.data["colorspace"] = None
