4f799c30a22d971330f4c37ecbd95f8d4bddf481
maxpareschi <max.pareschi@gmail.com>
2024-08-16 16:07:23 +0200

    bumping up version 97

--- openpype/version.py
+++ openpype/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 """Package declaring Pype version."""
-# __version__ = "3.14.11-22dogs.96"
-__version__ = "3.14.11-22dogs.96+staging"
+# __version__ = "3.14.11-22dogs.97"
+__version__ = "3.14.11-22dogs.97+staging"
