c5e57edf0d87293fb9ce6eb76b18c0b90176fd93
maxpareschi <max.pareschi@gmail.com>
2024-10-03 12:35:31 +0200

    bump vertsion to v117 - v117+staging

--- openpype/version.py
+++ openpype/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 """Package declaring Pype version."""
-__version__ = "3.14.11-22dogs.116"
-# __version__ = "3.14.11-22dogs.116+staging"
+__version__ = "3.14.11-22dogs.117"
+# __version__ = "3.14.11-22dogs.117+staging"
