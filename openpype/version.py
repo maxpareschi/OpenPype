adea74180e141b6dd5d29b1c00d537f7343e657f
Max Pareschi <max.pareschi@gmail.com>
2024-10-23 18:03:08 +0200

    bump version to v130 - v130+staging

--- openpype/version.py
+++ openpype/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 """Package declaring Pype version."""
-__version__ = "3.14.11-22dogs.129"
-# __version__ = "3.14.11-22dogs.129+staging"
+__version__ = "3.14.11-22dogs.130"
+# __version__ = "3.14.11-22dogs.130+staging"
