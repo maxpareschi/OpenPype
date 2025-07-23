884a9dbea4a26944cbbfc52c8a20f8288d344401
Max Pareschi <max.pareschi@gmail.com>
2025-07-23 17:51:01 +0200

    fix on task passing for people breaking up their env

--- openpype/version.py
+++ openpype/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 """Package declaring Pype version."""
-__version__ = "3.14.11-22dogs.157"
-# __version__ = "3.14.11-22dogs.157+staging"
+__version__ = "3.14.11-22dogs.159"
+# __version__ = "3.14.11-22dogs.159+staging"
